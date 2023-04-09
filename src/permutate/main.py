88d0a92bfca4844b8b32a097cb6ed8a3c88e027b
Kate Folk <folkenrothk@allegheny.edu>
2023-04-08 22:16:06 -0400

    chaotic save point. one day i will learn to back out of my folder first today is not that day

---
+++ src/permutate/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# remixed from "I AM THAT I AM" by Nick Manafort, 2014
+
+import string
+
+def permutate(elems):
+    if len(elems) == 0:
+        yield elems
+    else:
+        for result in permutate(elems[1:]):
+            for i in range(len(elems)):
+                yield result[:i] + elems[0:1] + result[i:]
+
+test_str = str(list(permutate('user'))).replace("'", '').replace(",",'').replace("]",'').replace("[",'')
+
+f = open("../../content/work001.txt", "x")
+f.write(test_str)
